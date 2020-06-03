953271ed77c7e92d4c6f526da2939d06668d6562
Katie Gross <ktgross15@gmail.com>
2020-06-03 11:25:19 -0400

    added comments

--- utilities.py
+++ utilities.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 def multiply_cols(df, x, y, new_col_name):
+    '''mutliply two columns together and return new dataframe'''
     df[new_col_name] = df[x] * df[y]
     return df
